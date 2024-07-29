1aa2759117ae28fe292d3d42ed7c1ed8605157ff
whorajan <rajan_k2@yahoo.com>
2024-07-29 12:58:56 +0530

    put, get, delete endpoints

--- testcaseController.py
+++ testcaseController.py
@@ -6,3 +6,15 @@ app = FastAPI()
 @app.post("/testcase/")
 async def posttestcase(context: testcaseRequestContext):
    return {"testcase":{"case1":"testcase1", "case2":"testcase2"}, "testscript":{"case1":"testcase1", "case2":"testcase2"}}
+
+@app.get("/testcase/{testcase_id}")
+async def gettestcase(testcase_id: str):
+   return {"testcase_id": testcase_id};
+
+@app.put("/testcase/{testcase_id}")
+async def puttestcase(testcase_id: str):
+   return {"testcase_id": testcase_id};
+
+@app.delete("/testcase/{testcase_id}")
+async def deletetestcase(testcase_id: str):
+   return {"testcase_id": testcase_id};
